f9e4c3628e8c8edfe8867e42e1fdc30f243f9b7d
j <hong@cloudjetkpi.com>
2025-05-24 15:29:23 +0700

    config

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -10,15 +10,15 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://james.github.io',
+  url: 'https://cschanhniem.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/the-open-path/',
+  baseUrl: '/TheOpenPath/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'james', // Usually your GitHub org/user name.
-  projectName: 'the-open-path', // Usually your repo name.
+  organizationName: 'cschanhniem', // Usually your GitHub org/user name.
+  projectName: 'TheOpenPath', // Usually your repo name.
 
   onBrokenLinks: 'warn', // Temporarily set to warn to allow build with translation issues
   onBrokenMarkdownLinks: 'warn',
